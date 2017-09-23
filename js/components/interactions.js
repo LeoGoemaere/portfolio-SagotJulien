d4d2e419dd305a70792a853162a9ecc6c38b80d9
GOEMAERE <leo.goemaere@capgemini.com>
2017-09-23 19:29:10 +0200

    Full screen videos on mobile

--- js/components/interactions.js
+++ js/components/interactions.js
@@ -2,19 +2,43 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	const buttons = Array.from(document.querySelectorAll('.js-cta'));
 	const animateDevice = document.querySelector('.animate-device');
+	const wrapperVideos = document.querySelector('.videos');
+	const videos = Array.from(wrapperVideos.querySelectorAll('video'));
 	const overlay  = document.querySelector('.overlay');
 	const breakpoint = 768;
 	let isActive = false;
 
-	function insertDatasInAnimateDevice(animateDevice, deviceType, videoName) {
-		let chosenVideo = animateDevice.querySelector(`.js-${videoName}`);
+	console.log(videos);
+	// function insertDatasInAnimateDevice(animateDevice, deviceType, videoName) {
+	// 	let chosenVideo = animateDevice.querySelector(`.js-${videoName}`);
+	// 	animateDevice.classList.add(deviceType);
+	// 	animateDevice.classList.add('activated');
+	// 	animateDevice.classList.remove('desactivated');
+	// 	animateDevice.dataset.device = deviceType;
+	// 	chosenVideo.classList.add('active');
+	// 	chosenVideo.play();	// When datas are insert we play the video
+	// 	isActive = true;
+	// }
+
+	function insertDatasInAnimateDevice(animateDevice, videosContainer, deviceType, videoName) {
+		let chosenVideo = videosContainer.querySelector(`.js-${videoName}`);
+		let animateVideo = animateDevice.querySelector('video');
 		animateDevice.classList.add(deviceType);
 		animateDevice.classList.add('activated');
 		animateDevice.classList.remove('desactivated');
+		console.log(chosenVideo);
 		animateDevice.dataset.device = deviceType;
-		chosenVideo.classList.add('active');
-		chosenVideo.play();	// When datas are insert we play the video
+
+		animateVideo.src = chosenVideo.dataset.src;
+		animateVideo.play();	// When datas are insert we play the video
 		isActive = true;
+	}
+
+	function playFullScreen(videosContainer, videoName) {
+		let chosenVideo = videosContainer.querySelector(`.js-${videoName}`);
+		chosenVideo.load();
+		chosenVideo.classList.add('active');
+		chosenVideo.play();
 	}
 
 	function desactivateAnimation(animateDevice) {
@@ -23,12 +47,21 @@ window.addEventListener('DOMContentLoaded', () => {
 		isActive = false;
 	}
 
+	// function removeAttributesIfAnimationEnd(targetElement, classToRemove) {
+	// 	let activeVideo = targetElement.querySelector("video.active");
+	// 	targetElement.classList.remove(classToRemove);
+	// 	targetElement.dataset.device = '';
+	// 	activeVideo.classList.remove('active');
+	// 	activeVideo.load();	// Stop the video when animation end
+	// }
+
 	function removeAttributesIfAnimationEnd(targetElement, classToRemove) {
-		let activeVideo = targetElement.querySelector("video.active");
+		let animateVideo = targetElement.querySelector("video");
+		animateVideo.src = '';
 		targetElement.classList.remove(classToRemove);
 		targetElement.dataset.device = '';
-		activeVideo.classList.remove('active');
-		activeVideo.load();	// Stop the video when animation end
+		// activeVideo.classList.remove('active');
+		// activeVideo.load();	// Stop the video when animation end
 	}
 
 	function getDevice(targetElement) {
@@ -49,10 +82,18 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	buttons.map(button => button.addEventListener('click', function() { 
 		if (window.innerWidth > breakpoint) {
-			insertDatasInAnimateDevice(animateDevice, getDevice(this), getVideoName(this));
+			insertDatasInAnimateDevice(animateDevice, wrapperVideos, getDevice(this), getVideoName(this));
 			playAnimationOverlay(overlay, 'off', 'active', 'inactive');
+		} else {
+			playFullScreen(wrapperVideos, getVideoName(this));
 		}
 	}))
+
+	videos.map(video => video.addEventListener('pause', function() { 
+		this.classList.remove('active');
+	}))
+
+
 
 	overlay.addEventListener('click', function() {
 			desactivateAnimation(animateDevice);
