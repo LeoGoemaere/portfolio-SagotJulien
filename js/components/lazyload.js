31ea2dc43facd747c9df7457fb01b07514184320
GOEMAERE <leo.goemaere@capgemini.com>
2017-09-24 20:04:32 +0200

    Add all assets

--- js/components/lazyload.js
+++ js/components/lazyload.js
@@ -1,15 +1,10 @@
-window.addEventListener('load', () => {
+window.addEventListener('DOMContentLoaded', () => {
 
 	const lazyImages = Array.from(document.querySelectorAll('.lazy-image'));
-	const lazyVideos = Array.from(document.querySelectorAll('.lazy-video'));
 
 	lazyImages.map((image) => {
 		image.src = image.dataset.src;
 		image.srcset = image.dataset.srcset; 
-	})
-
-	lazyVideos.map((video) => {
-		video.src = video.dataset.src;
 	})
 
 })
