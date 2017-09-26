5611f44e7db3656408cecb781de526bd5a613ad0
Julien Sagot <contact@juliensagot.fr>
2017-09-26 17:42:10 +0200

    Disable scrollreveal on mobile

--- js/components/playScrollReveal.js
+++ js/components/playScrollReveal.js
@@ -1,11 +1,14 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-	window.sr = ScrollReveal({ 
-		origin: 'top',
-		duration: 600,
-		easing: 'ease-out',
-		distance: '4%',
-	});
-	sr.reveal('.js-sr');
+	const breakpoint = 768;
 
+	if (window.innerWidth > breakpoint) {
+		window.sr = ScrollReveal({ 
+			origin: 'top',
+			duration: 600,
+			easing: 'ease-out',
+			distance: '4%',
+		});
+		sr.reveal('.js-sr');
+	}
 })
