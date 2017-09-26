5b82c858b37765cec2f155953d1180b5a0ae91ce
Julien Sagot <contact@juliensagot.fr>
2017-09-26 23:37:39 +0200

    Bring back lazysizes for videos

--- js/components/lazyload.js
+++ js/components/lazyload.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', () => {
 
-	const lazyImages = Array.from(document.querySelectorAll('.lazyload'));
+	const lazyImages = Array.from(document.querySelectorAll('.lazy-image'));
 
 	lazyImages.map((image) => {
 		image.src = image.dataset.src;
